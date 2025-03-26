db78c68e7b2a3760cd1d9b6623cbf52ae832303b
Arthur <arthurrochadx@gmail.com>
2025-03-25 21:01:42 -0300

    funcionando para 5 paginas simultaneas

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ const sleep = (ms) => {
         setTimeout(resolve, ms));
 }
 
-const scrapePage = async () => {
-    let currentPage = 1;
+initScraping = async () => {
+    const startTime = Date.now();
     const browser = await puppeter.launch({headless: false});
+    const promisesArray = [];
+    for(let i = 1; i <= 5; i++){
+        promisesArray.push(scrapePage(browser, i));
+    }
+    const results =  await Promise.all(promisesArray);
+    console.log(results.flat());
+    console.log('Execution time: ', Date.now() - startTime);
+    await browser.close();
+    writeFileSync('output.json', JSON.stringify(results.flat(), null, 2));
+    
+}
+
+const scrapePage = async (browser, currentPage) => {
+
     const page = await browser.newPage();
 
     await page.goto('https://mercado.carrefour.com.br/bebidas?page=' + currentPage, {
-        waitUntil: 'networkidle2',
+        waitUntil: 'domcontentloaded',
         timeout: 15000
     });
     
@@ -21,30 +35,29 @@ const scrapePage = async () => {
 
     //recarregando a página, o menu para aceitar os cookies esconde o botão de inserir o CEP
 
-    await page.reload({waitUntil: 'networkidle2', timeout: 15000});
+    // await page.reload({waitUntil: 'networkidle2', timeout: 15000});
 
-    await page.locator('button[title= "Insira seu CEP"]').click();
+    await page.locator('button[title= "Insira seu CEP"]')?.click();
 
-    await page.locator('button[role = "tab"] ::-p-text(Retire na Loja)').click();
+    await page.locator('button[role = "tab"] ::-p-text(Retire na Loja)')?.click();
 
-    await page.locator('select[id = "selectCity"]').click();
+    await page.locator('select[id = "selectCity"]')?.click();
 
     await page.select('select[id = "selectCity"]', 'Piracicaba');
 
-    await page.locator('article[role = "presentation"]').click();
+    await page.locator('article[role = "presentation"]')?.click();
 
-    await sleep(1500);
+    await sleep(30);
 
     const content = await page.evaluate(() => {
         const products = [];
         const productElements = document.querySelectorAll('section[data-testid="store-product-card-content"]');
         for(const productElement of productElements){
-            const productName = productElement.querySelector('h3').innerText;
-            const productPrice = productElement.querySelector('span[data-test-id="price"]').innerText;
-            const productOnSale = productElement.querySelector('.mb-2').innerText;
+            const productName = productElement.querySelector('h3')?.innerText;
+            const productPrice = productElement.querySelector('span[data-test-id="price"]')?.innerText;
+            const productOnSale = productElement.querySelector('.mb-2')?.innerText;
 
             products.push({
-                "id": products.length + 1,
                 "title": productName,
                 "price": productPrice,
                 "onSale": productOnSale ? productOnSale : false
@@ -52,13 +65,13 @@ const scrapePage = async () => {
         }
         return products;
     });
+   
+    return content;
     
-    console.log(content, content.length);
-    writeFileSync('output.json', JSON.stringify(content, null, 2));
 
-    await browser.close();
+    
     
     
 }
 
-scrape();
+initScraping();
